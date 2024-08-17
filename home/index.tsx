e133e8d8c92064d86b26696887c0f6a47e6e885e
Oleksandr Solontsov <solontsov@gmail.com>
2024-08-17 20:30:19 +0100

    change <ul> to <table>

--- home/index.tsx
+++ home/index.tsx
@@ -9,6 +9,21 @@ import { Recipe } from '../metadata/recipes';
 
 import { RecipeForm } from './Components/RecipeForm';
 
+//visibility test
+const ComponentA = ({ switchToB }) => (
+  <div>
+    <h1>Component A</h1>
+    <button onClick={switchToB}>Switch to Component B</button>
+  </div>
+);
+
+const ComponentB = ({ switchToA }) => (
+  <div>
+    <h1>Component B</h1>
+    <button onClick={switchToA}>Switch to Component A</button>
+  </div>
+);
+
 function App() {
 
   const [recipes, setRecipes] = useState<Recipe[]>([]);
@@ -23,23 +38,67 @@ function App() {
 
   }
 
+  const deleteRecipe = () => {
+
+  }
+
   useEffect(() => updateRecipesList(), []);
+
+  //test
+  const [showRecipeForm, setShowComponentA] = useState(true);
+
+  const setShowRecipeForm = () => setShowComponentA(true);
+  const processRecipeFormClose = () => setShowComponentA(false);
 
 
   return (
+    <>
     <div className="rec-list">
       <h2>Recipes list</h2>
       <nav>
         <button onClick={updateRecipesList}>Update</button>
-        <button onClick={addRecipe}>Add</button></nav>
-      <ul>
-        {recipes.map((item, i) => (
-          <li key={i} className="list-item">
-            name = {item.name}; prepTimeInMinutes = {item.prepTimeInMinutes}
-          </li>))}
-      </ul>
+        <button onClick={addRecipe}><span style={{ marginRight: '0.5em' }}>➕</span>Add</button>
+        <button onClick={deleteRecipe}><span style={{ marginRight: '0.5em' }}>🗑</span>Delete</button>
+      </nav>
+
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>Name</th>
+            <th>Prep. time (min)</th>
+            <th>Ingredients</th>
+          </tr>
+        </thead>
+        <tbody>
+
+          {recipes.map((item, i) => (
+            <tr key={i} className="list-item">
+              <td>
+                <input type="checkbox"  className='checkbox'/>
+              </td>
+              <td>
+                {item.name}
+              </td>
+              <td>
+                {item.prepTimeInMinutes}
+              </td>
+              <td>
+
+              </td>
+            </tr>))}
+        </tbody>
+      </table>
       <RecipeForm />
     </div>
+
+    {showRecipeForm ? (
+        <ComponentA switchToB={processRecipeFormClose} />
+      ) : (
+        <ComponentB switchToA={setShowRecipeForm} />
+      )}
+
+    </>
   );
 }
 
--- todo.md
+++
@@ -1,9 +0,0 @@
-## Add recipe
- - open RecipeForm for entering data
- - update list if a new recipe is saved
-
-## Edit recipe
-- add button that opens RecipeForm
-
-## Interface
-- Table view
